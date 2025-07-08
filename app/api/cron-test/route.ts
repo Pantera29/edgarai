cb6fa9544cd4340736f85d1a15edebf476fbe0a9
Franco Longhi <francolonghi29@gmail.com>
2025-07-08 11:47:54 -0600

    configuracion cron en github actions

--- app/api/cron-test/route.ts
+++ app/api/cron-test/route.ts
@@ -1,7 +1,7 @@
 export const dynamic = 'force-dynamic';
 
 export async function GET(request: Request) {
-  console.log('🔔 Cron job started');
+  console.log('🔔 [CRON-TEST] Ejecución recibida en /api/cron-test');
   console.log('Headers:', JSON.stringify(Object.fromEntries(request.headers)));
   return new Response('Cron test OK', { status: 200 });
 } 
