ad7bea5af98ef9945f04a50d50428939f52be429
Franco Longhi <francolonghi29@gmail.com>
2025-10-28 18:15:58 -0600

    Eliminar export de ConversationList del index
    - Remover exportación del componente ConversationList eliminado
    - Corregir error de build en producción

--- components/whatsapp-layout/index.ts
+++ components/whatsapp-layout/index.ts
@@ -1,4 +1,3 @@
 export { WhatsAppStyleLayout } from './WhatsAppStyleLayout';
-export { ConversationList } from './ConversationList';
 export { ChatPanel } from './ChatPanel';
 export { ChatPlaceholder } from './ChatPlaceholder';
